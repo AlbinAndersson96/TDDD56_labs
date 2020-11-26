03479826951b810f0080b5bf1f98af609417e7d3
Seltiix <filkl010@student.liu.se>
2020-11-26 11:37:13 +0100

    asd

--- Lab3/median.cpp
+++ Lab3/median.cpp
@@ -33,7 +33,7 @@ unsigned char median_kernel(skepu::Region2D<unsigned char> image, size_t elemPer
 			hold[arrCounter++] = image(y, x);
 
     for (int i = 0; i < arrCounter-1; ++i) {  
-		for (int j = 0; j < arrCounter-i-1; ++j) {
+		for (int j = i+1; j < arrCounter-1; ++j) {
 			if (hold[j] > hold[j+1]) {
 				float temp = hold[j];  
 				hold[j] = hold[j+1];  
