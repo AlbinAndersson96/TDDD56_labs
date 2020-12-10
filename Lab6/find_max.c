2bc7553662b73874561195895a04d6688c552d9b
Albin Andersson <albin-andersson1996@hotmail.com>
2020-12-10 14:49:40 +0100

    auto

--- Lab6/find_max.c
+++ Lab6/find_max.c
@@ -26,7 +26,7 @@
 #include "milli.h"
 
 // Size of data!
-#define kDataLength 268435456
+#define kDataLength 134217728
 #define MAXPRINTSIZE 16
 
 unsigned int *generateRandomData(unsigned int length)
