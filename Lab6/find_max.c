07b06a7ae707d06453b1684d9deb6f24f4050a28
Albin Andersson <albin-andersson1996@hotmail.com>
2020-12-11 11:41:10 +0100

    auto

--- Lab6/find_max.c
+++ Lab6/find_max.c
@@ -106,33 +106,33 @@ int find_max_gpu(unsigned int *data, unsigned int length)
 	printf("GPU reduction.\n");
 
   int numberOfRuns = 1;
-  if (kDataLength > 16384) numberOfRuns = (kDataLength / 16384) + 1;
+  if (kDataLength > 32768) numberOfRuns = (kDataLength / 32768) + 1;
 
   unsigned int maxRuns[numberOfRuns];
 
-  unsigned int partData[16384];
-  io_data = clCreateBuffer(cxGPUContext, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR, 16384 * sizeof(unsigned int), partData, &ciErrNum);
+  unsigned int partData[32768];
+  io_data = clCreateBuffer(cxGPUContext, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR, 32768 * sizeof(unsigned int), partData, &ciErrNum);
 
   cl_event eventReadBuffer, eventWriteBuffer;
   ResetMilli();
   for(int i = 0; i < numberOfRuns; ++i) {
     
-    for(int dataIndex = 0; dataIndex < 16384; ++dataIndex)
+    for(int dataIndex = 0; dataIndex < 32768; ++dataIndex)
     {
-      partData[dataIndex] = data[i*16384 + dataIndex];
+      partData[dataIndex] = data[i*32768 + dataIndex];
     }
 
-    ciErrNum = clEnqueueWriteBuffer(commandQueue, io_data, CL_TRUE, 0, 16384*sizeof(unsigned int), partData, 0, NULL, &eventWriteBuffer);
+    ciErrNum = clEnqueueWriteBuffer(commandQueue, io_data, CL_TRUE, 0, 32768*sizeof(unsigned int), partData, 0, NULL, &eventWriteBuffer);
     clWaitForEvents(1, &eventWriteBuffer);
   	
 	  //printCLError(ciErrNum,7);
 
 	  // ********** RUN THE KERNEL ************
-	  runKernel(gpgpuReduction, 16384, io_data, 16384);
+	  runKernel(gpgpuReduction, 32768, io_data, 32768);
 
 	  // Get data
  
-	  ciErrNum = clEnqueueReadBuffer(commandQueue, io_data, CL_TRUE, 0, 16384 * sizeof(unsigned int), partData, 0, NULL, &eventReadBuffer);
+	  ciErrNum = clEnqueueReadBuffer(commandQueue, io_data, CL_TRUE, 0, 32768 * sizeof(unsigned int), partData, 0, NULL, &eventReadBuffer);
 	  //printCLError(ciErrNum,11);
     clWaitForEvents(1, &eventReadBuffer);
 
