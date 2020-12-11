e7e72cb00caa19d35ca408d6ecd9051b38755001
Albin Andersson <albin-andersson1996@hotmail.com>
2020-12-11 12:53:08 +0100

    auto

--- Lab6/find_max.c
+++ Lab6/find_max.c
@@ -28,9 +28,13 @@
 #define MAXPRINTSIZE 16
 
 // Size of data!
+#define kDataLength 4194304
+//#define kDataLength 8388608
+//#define kDataLength 16777216
+//#define kDataLength 33554432
 //#define kDataLength 67108864
 //#define kDataLength 268435456
-#define kDataLength 1073741824*2
+//#define kDataLength 1073741824
 
 // #define THREADS 256
 // #define PART_SIZE 16384
